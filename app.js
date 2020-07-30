ace00d576318854f8ab66c1b2d00d70bd58c5130
Nick Mallas <nmallas23@gmail.com>
2020-07-30 15:42:34 -0500

    add-page path

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
 const routes = require('./routes');
 
@@ -10,6 +11,9 @@ const app = express();
 app.set('view engine', 'pug');
 
 app.use(morgan('dev'));
+
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}));
 
 app.use(routes);
 
--- routes.js
+++ routes.js
@@ -1,7 +1,58 @@
 const express = require("express");
+const csrf = require('csurf');
+
 const router = express.Router();
-router.get('/', (req, res) => {
-    res.render('index', { title: 'Home' });
+const db = require("./db/models");
+
+const csrfProtection = csrf({cookie: true});
+
+const asyncHandler = (handler)=> {
+    return function(req, res, next) {
+        return handler(req, res, next).catch(next);
+    }
+}
+router.get('/', asyncHandler(async (req, res) => {
+        const books = await db.Book.findAll({order: [['title', 'ASC']]});
+        res.render('book-list', { title: 'Books', books});
+  }));
+
+ router.get('/book/add', csrfProtection, (req, res) => {
+  const book = db.Book.build();
+  res.render('book-add', {
+    title: 'Add Book',
+    book,
+    csrfToken: req.csrfToken(),
   });
+});
+
+router.post('/book/add', csrfProtection, asyncHandler(async (req, res) => {
+  const {
+    title,
+    author,
+    releaseDate,
+    pageCount,
+    publisher,
+  } = req.body;
+
+  const book = db.Book.build({
+    title,
+    author,
+    releaseDate,
+    pageCount,
+    publisher,
+  });
+
+  try {
+    await book.save();
+    res.redirect('/');
+  } catch (err) {
+    res.render('book-add', {
+      title: 'Add Book',
+      book,
+      error: err,
+      csrfToken: req.csrfToken(),
+    });
+  }
+}));
 
 module.exports = router;
